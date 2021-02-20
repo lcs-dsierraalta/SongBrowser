70b381fb00342f03f89c478c2b65249ee3c2800b
Diego Sierraalta <dsierraalta@lcs.on.ca>
2021-02-20 10:15:39 -0500

    Defined the model for the parts of the JSON part we care about retrieving

---
+++ SongBrowser/Model/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  SongBrowser
+//
+//  Created by Diego Sierraalta on 2021-02-20.
+//
+
+import Foundation
+
+struct SearchResult: Decodable {
+    
+    let results: [Song]
+    
+}
